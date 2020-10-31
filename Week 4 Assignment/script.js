f47ff0ff73fe14983f388312d33b2ea27d8530b0
Sajib Bain <69715246+SajibBain@users.noreply.github.com>
2020-10-31 13:10:21 +0600

    Create script.js

---
+++ Week 4 Assignment/script.js
@@ -0,0 +1,23 @@
+var names = [
+  "Yaakov",
+  "John",
+  "Jen",
+  "Jason",
+  "Paul",
+  "Frank",
+  "Larry",
+  "Paula",
+  "Laura",
+  "Jim",
+];
+
+for (var i = 0; i < names.length; i++) {
+  var name = names[i];
+  var firstLetter = name[0].toLocaleLowerCase();
+
+  if (firstLetter == "j") {
+    byeSpeaker.speak(name);
+  } else {
+    helloSpeaker.speak(name);
+  }
+}
